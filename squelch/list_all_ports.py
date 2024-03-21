1503f937a6e3e1446a88e05a44808e112ffbe283
mvyanakiev <myanakiev@gmail.com>
2024-03-21 18:13:32 +0200

    Refactor

--- squelch/list_all_ports.py
+++ squelch/list_all_ports.py
@@ -1,14 +1,14 @@
 from serial.tools import list_ports
 
-correctPort = 'Not found'
+correct_port = 'Not found'
 
 ports = list(list_ports.comports())
-i = 0
+i = 1
 for p in ports:
 
     if p.description == 'USB2.0-Serial':
-        correctPort = p.device
+        correct_port = p.device
     print('Port ' + str(i) + ' from total ' + str(len(ports)) + ' is ' + p.device)
     i += 1
 
-print('Serial port is: ' + correctPort)
+print('Serial port is: ' + correct_port)
